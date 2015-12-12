53c32c73ebf8032ea59183b3f9e107910ab23980
Riko Ophorst <riko_ophorst@hotmail.com>
2015-12-12 05:37:19 +0100

    Got the basic mechanics of the game down

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    private List<World> worlds;
+
+    public void Awake()
+    {
+        worlds = new List<World>();
+
+        Object worldResource = Resources.Load("World");
+
+        GameObject[] sceneWorlds = GameObject.FindGameObjectsWithTag("World");
+        for (int i = 0; i < sceneWorlds.Length; i++)
+        {
+            worlds.Add(sceneWorlds[i].GetComponent<World>());
+        }
+    }
+
+    public World FindClosestWorld(Vector3 position)
+    {
+        World closestWorld = null;
+        float closestDistance = Mathf.Infinity;
+
+        float distance;
+
+        for (int i = 0; i < worlds.Count; i++)
+        {
+            distance = GetBorderDistance(worlds[i], position);
+
+            if (closestWorld == null || distance < closestDistance)
+            {
+                closestWorld = worlds[i];
+                closestDistance = distance;
+            }
+        }
+
+        return closestWorld;
+    }
+
+    public List<World> FindInGravityRadius(Vector3 position)
+    {
+        List<World> worldsInRange = new List<World>();
+
+        for (int i = 0; i < worlds.Count; i++)
+        {
+            if (Vector3.Distance(position, worlds[i].transform.position) <= worlds[i].gravityRadius)
+            {
+                worldsInRange.Add(worlds[i]);
+            }
+        }
+
+        return worldsInRange;
+    }
+
+    private float GetBorderDistance(World world, Vector3 position)
+    {
+        return (world.gameObject.transform.position - position).magnitude - world.gravityRadius;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    [HideInInspector]public List<World> currentWorlds;
+
+    private GameController gameController;
+
+    public void Awake()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    public void FixedUpdate()
+    {
+        currentWorlds = gameController.FindInGravityRadius(transform.position);
+
+        Vector3 worldPosition;
+        Vector3 position;
+
+        for (int i = 0; i < currentWorlds.Count; i++)
+        {
+            worldPosition = currentWorlds[i].transform.position;
+            position = transform.position;
+
+            Debug.DrawLine(position, worldPosition);
+
+            float distance = Vector3.Distance(position, worldPosition);
+
+            if (distance < currentWorlds[i].gravityRadius)
+            {
+                Vector3 newPosition = Vector3.MoveTowards(position, worldPosition, currentWorlds[i].gravityStrength * (1.0f - distance / currentWorlds[i].gravityRadius));
+                float newDistance = Vector3.Distance(newPosition, worldPosition);
+
+                if (newDistance < currentWorlds[i].surfaceRadius)
+                {
+                    float angle = Mathf.Atan2(position.y - worldPosition.y, position.x - worldPosition.x);
+                    transform.position = worldPosition + new Vector3(Mathf.Cos(angle) * currentWorlds[i].surfaceRadius, Mathf.Sin(angle) * currentWorlds[i].surfaceRadius);
+                }
+                else
+                {
+                    transform.position = newPosition;
+                }
+            }
+        }
+    }
+}
