bd939ae841fb312b4a3c81ea82bbb925c861f862
Riko Ophorst <riko_ophorst@hotmail.com>
2015-12-13 10:16:40 +0100

    Pickups now gravitate towards the player

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,15 +5,19 @@ using System.Collections;
 public class Pickup : MonoBehaviour
 {
     public World.Resources type;
+    public Player player;
 
     public void Awake()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
     public void Update()
     {
-
+        if (Vector3.Distance(player.transform.position, transform.position) < player.pickUpRadius)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, (1.0f - (Vector3.Distance(player.transform.position, transform.position) / player.pickUpRadius)) * player.pickUpStrength);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collider)
