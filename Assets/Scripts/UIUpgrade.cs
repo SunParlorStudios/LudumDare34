859b2d4088ffb8c52a1748278b6c668709ac823a
Daniël Konings <konings_304@hotmail.com>
2015-12-14 17:05:10 +0100

    More HUD stuff, the upgrade window is close to done

---
+++ Assets/Scripts/UIUpgrade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class UIUpgrade : MonoBehaviour
+{
+    private GameObject checkMark;
+    private GameObject cross;
+    public GameObject selectionBox;
+
+    bool hasUpgrade;
+
+	void Awake()
+    {
+        checkMark = gameObject.transform.GetChild(0).gameObject;
+        cross = gameObject.transform.GetChild(1).gameObject;
+
+        hasUpgrade = false;
+    }
+
+    public void OnPointerEnter(BaseEventData evt)
+    {
+        selectionBox.SetActive(true);
+        selectionBox.transform.position = transform.position;
+    }
+
+    public void OnPointerExit(BaseEventData evt)
+    {
+        selectionBox.SetActive(false);
+    }
+
+        void Update()
+    {
+        checkMark.SetActive(hasUpgrade);
+        cross.SetActive(!hasUpgrade);
+	}
+}
